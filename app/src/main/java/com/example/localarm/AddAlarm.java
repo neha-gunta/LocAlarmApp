6cb1403122a0a9618e29328aed121dcf8cd1a6fe
VAISHNAVI <vaishnavishreshthi>
2022-11-30 16:13:44 +0530

    add ui

--- app/src/main/java/com/example/localarm/AddAlarm.java
+++ app/src/main/java/com/example/localarm/AddAlarm.java
@@ -2,13 +2,27 @@ package com.example.localarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
 
 public class AddAlarm extends AppCompatActivity {
+    private static final int PICK_CONTACT = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_alarm);
+
+        Button button = findViewById(R.id.button5);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(intent,PICK_CONTACT);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/localarm/Settings.java
+++ app/src/main/java/com/example/localarm/Settings.java
@@ -2,7 +2,10 @@ package com.example.localarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Settings extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        Button button = findViewById(R.id.button8);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Settings.this,EmergencyContacts.class);
+                startActivity(intent);
+            }
+        });
     }
 }
